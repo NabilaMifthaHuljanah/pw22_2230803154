358515db4a7fdb0a66a0a08278d378e0c4a18e5c
NabilaMifthaHuljanah <145323205+NabilaMifthaHuljanah@users.noreply.github.com>
2023-11-10 10:16:18 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,20 @@
+<?php
+
+$a "hello"; 
+$hello= "Hello world!";
+
+// menampilkan isi variabel $a 
+// hello 
+echo $a . "</br>";
+
+// menampilkan isi variabel $a 
+// Hello world! 
+echo Shello."</br>";
+
+// menampilkan isi dari variabel dengan nama yang sama seperti isi variabel $a 
+// isi variabel sa hello. jadi, nanti akan menampilkan isi dari variabel 
+Shello
+// Hello world
+echo $sa. "</br>";
+
+?>
